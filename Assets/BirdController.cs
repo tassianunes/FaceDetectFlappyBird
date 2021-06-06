f65a2e820c80ece69363d58c1a4945e7f63bfdb3
tassianunes <tassia.nunes@edu.isd.org.br>
2021-06-06 09:48:42 -0300

    Adjustment scene is working

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
+   
     FaceDetector faceDetector;
     [SerializeField] float speed;
     float lastY = 0;
@@ -12,14 +13,15 @@ public class BirdController : MonoBehaviour
     {
        
         faceDetector = (FaceDetector)FindObjectOfType(typeof(FaceDetector));
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // float step = speed * Time.deltaTime;
-        // float norm = Mathf.Clamp(faceDetector.faceY - lastY, -1, 1);
+        //float step = speed * Time.deltaTime;
+        //float norm = Mathf.Clamp(faceDetector.faceY - lastY, -1, 1);
 
         //transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + norm, transform.position.z), step);
 
@@ -28,11 +30,15 @@ public class BirdController : MonoBehaviour
         //transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, -faceDetector.faceY, transform.position.z), step);
 
         //transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, -faceDetector.faceY - norm, transform.position.z), step);
-        // transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, -transform.position.y + norm, transform.position.z), step);
-        Vector3 movement = new Vector3(transform.position.x, -faceDetector.faceY/67f + 3, 0f);
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, -transform.position.y + norm, transform.position.z), step);
 
+        Vector3 movement = new Vector3(transform.position.x, -faceDetector.faceY/30f + 3, 0f);
         transform.position = Vector3.MoveTowards(transform.position, movement, speed * Time.deltaTime);
+        
+
     }
+
+
 }
 
 
